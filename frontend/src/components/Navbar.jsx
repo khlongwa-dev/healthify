08f39d5c32cc2e54a2e53ca47038ab7e9e4ae65f
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-04-26 21:14:33 +0200

    feat(wip): added styling on a navbar.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,47 +25,47 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div>
-      <img onClick={() => navigate('/')} src={assets.logo} alt="" />
-      <ul>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+      <img onClick={() => navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
-          <li>HOME</li>
-          <hr />
+          <li className='py-1'>HOME</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/doctors'>
-          <li>ALL DOCTORS</li>
-          <hr />
+          <li className='py-1'>ALL DOCTORS</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/about'>
-          <li>ABOUT</li>
-          <hr />
+          <li className='py-1'>ABOUT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/contacts'>
-          <li>CONTACTS</li>
-          <hr />
+          <li className='py-1'>CONTACTS</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
       </ul>
       
-      <div>
+      <div className='flex items-center gap-4'>
         {
           token
-          ? <div>
-            <img src={assets.profile_pic} alt="" />
-            <img src={assets.dropdown_icon} alt="" />
+          ? <div className='flex items-center gap-2 cursor-pointer relative'>
+            <img className='w-8 rounded-full' src={assets.profile_pic} onClick={toggleDropdown} alt="" />
+            <img className='w-2.5' src={assets.dropdown_icon} onClick={toggleDropdown} alt="" />
             {dropdownOpen && (
-              <div ref={dropdownRef}>
-                <div>
-                  <p onClick={() => { navigate('/my-profile'); setDropdownOpen(false); }}></p>
-                  <p onClick={() => { navigate('/my-appointments'); setDropdownOpen(false); }}></p>
-                  <p onClick={() => { setToken(false); setDropdownOpen(false); }}></p>
+              <div ref={dropdownRef} className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20'>
+                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                  <p onClick={() => { navigate('/my-profile'); setDropdownOpen(false); }} className='hover:text-black cursor-pointer'>My Profile</p>
+                  <p onClick={() => { navigate('/my-appointments'); setDropdownOpen(false); }} className='hover:text-black cursor-pointer'>My Appointments</p>
+                  <p onClick={() => { setToken(false); setDropdownOpen(false); }}>Logout</p>
                 </div>
               </div>
             )}
           </div>
-          : <button onClick={() => navigate('/login')} >Create Account</button>
+          : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer' >Create Account</button>
         }
 
-        <img onClick={() => setShowMenu(true)} src={assets.menu_icon} alt="" />
+        <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
       </div>
     </div>
   )
