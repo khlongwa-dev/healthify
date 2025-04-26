ac77b31c02b2701720b0ec919c7131f62b5866e8
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-04-26 20:36:07 +0200

    feat(wip): implemented event listening for mouse clicks in navbar profile section.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { assets } from '../assets/assets';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [token, setToken] = useState(true);
+  const dropdownRef = useRef(null); // Create a ref for the dropdown
+
+  useEffect(()=> {
+    const handleClickOustide = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOustide);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOustide);
+    };
+  }, []);
+
   return (
     <div>
       
