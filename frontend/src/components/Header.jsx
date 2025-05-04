228efe14878a1dde9ac5bc3132b5ee2fcef5d139
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-04 16:59:17 +0200

    feat(wip): add a left section of the header that will have the booking button and introduction text.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
     <div>
-      
+        {/* -------- Left Side -------- */}
+        <div>
+            <p>
+                Book Your Appointment <br /> With Trusted Doctors
+            </p>
+            <div>
+                <img src={assets.group_profiles} alt="" />
+                <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block' /> schedule your appointment hassle-free.</p>
+            </div>
+            <a href="#speciality">
+                Book an appointment <img src={assets.arrow_icon} alt="" />
+            </a>
+        </div>
     </div>
   )
 }
