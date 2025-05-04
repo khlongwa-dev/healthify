7cd12e7e5ff24cddde245802889dafb5cc250382
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-04 17:18:37 +0200

    chore: added an initialized speciality menu component file.

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { specialityData } from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const SpecialityMenu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SpecialityMenu
