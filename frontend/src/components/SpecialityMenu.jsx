69da7885f4544e4d25a11e8efa72716ed387029f
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-04 17:22:35 +0200

    feat(wip): implemented a Speciality Menu.

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -5,7 +5,16 @@ import { Link } from 'react-router-dom'
 const SpecialityMenu = () => {
   return (
     <div>
-      
+      <h1>Find a Doctor By Speciality</h1>
+      <p>Simply browse through our extensive list of trusted doctors, <br /> schedule your appointment hassle-free.</p>
+      <div>
+        {specialityData.map((item, index)=>(
+            <Link onClick={()=>scrollTo(0,0)}>
+                <img src={item.image} alt="" />
+                <p>{item.speciality}</p>
+            </Link>
+        ))}
+      </div>
     </div>
   )
 }
