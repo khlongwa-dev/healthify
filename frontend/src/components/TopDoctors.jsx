09a5b6c9405f13526287ab53186b2fef55e61bd0
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-05 23:38:36 +0200

    feat: added a top doctors section.

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const TopDoctors = () => {
+    const navigate = useNavigate()
+    const {doctors} = useContext(AppContext)
+
   return (
     <div>
-      
+      <h1>Top Doctors to Book</h1>
+      <p>Simply browse through our extensive list of trusted doctors.</p>
+      <div>
+        {doctors.slice(0,10).map((item, index)=>(
+            <div  onClick={()=>{navigate(`/appointments/${item._id}`); scrollTo(0,0)}} key={index} >
+                <img src={item.image} alt="" />
+                <div>
+                    <div>
+                        <p></p><p>Available</p>
+                    </div>
+                    <p>{item.name}</p>
+                    <p>{item.speciality}</p>
+                </div>
+            </div>
+        ))}
+      </div>
+      <button onClick={()=>{ navigate('/doctors'); scrollTo(0,0)}}>more</button>
     </div>
   )
 }
