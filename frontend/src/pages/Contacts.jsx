131cba90731cb9348326d0f20d9f2bc25869dda0
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-06 15:33:54 +0200

    feat: implemented the contacts page.

--- frontend/src/pages/Contacts.jsx
+++ frontend/src/pages/Contacts.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contacts = () => {
   return (
     <div>
-      
+      <div>
+        <p>CONTACT <span>US</span></p>
+      </div>
+
+      <div>
+        <img src={assets.contact_image} alt="" />
+
+        <div>
+          <p>OUR OFFICE</p>
+          <p>00000 Willms Station <br /> Suite 000, Washington, USA</p>
+          <p>Tel: (000) 000-0000 <br /> Email: greatstackdev@gmail.com</p>
+          <p>CAREERS AT PRESCRIPTO</p>
+          <p>Learn more about our teams and job openings.</p>
+          <button>Explore Jobs</button>
+        </div>
+      </div>
     </div>
   )
 }
