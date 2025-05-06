9d8a81edbd7de414e625974d807a340a19676cef
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-06 15:02:18 +0200

    feat(wip): developed a left section of the banner.

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Banner = () => {
+    const navigate = useNavigate()
+
   return (
     <div>
-      
+        {/*--- left side --- */}
+      <div>
+        <div>
+            <p>Book Your Appointment</p>
+            <p>With 100+ Trusted Doctors</p>
+        </div>
+        <button onClick={()=>{navigate('/login'), scrollTo(0,0)}}></button>
+      </div>
     </div>
   )
 }
