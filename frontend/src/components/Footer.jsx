8d71d24c88f1453c4714f50c3c291e2c01576bf5
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-06 15:53:31 +0200

    feat(wip): implemented a left section of the footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-      
+      <div>
+        {/* --- left section --- */}
+        <div>
+            <img src={assets.logo} alt="" />
+            <p>Healthify is a convenient app for booking doctor appointments with ease. Healthify has become a go-to solution for patients seeking quick access to healthcare professionals, offering a seamless and efficient way to schedule medical visits anytime, anywhere.
+            </p>
+        </div>
+      </div>
     </div>
   )
 }
