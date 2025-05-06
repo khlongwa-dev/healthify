d5f4f13fb2c9c9ddfabf828979306260ff86831b
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-06 15:10:57 +0200

    feat: added the left section of the banner to complete it.

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -15,6 +15,11 @@ const Banner = () => {
         </div>
         <button onClick={()=>{navigate('/login'), scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create account</button>
       </div>
+
+      {/*--- right side --- */}
+      <div>
+        <img src={assets.appointment_img} alt="" />
+      </div>
     </div>
   )
 }
