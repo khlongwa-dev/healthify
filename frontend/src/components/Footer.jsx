46d4872092a10b8f9db48b291c4bcd51d0afea8a
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-06 15:55:54 +0200

    feat(wip): implemented the mid section of the footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,6 +11,17 @@ const Footer = () => {
             <p>Healthify is a convenient app for booking doctor appointments with ease. Healthify has become a go-to solution for patients seeking quick access to healthcare professionals, offering a seamless and efficient way to schedule medical visits anytime, anywhere.
             </p>
         </div>
+
+        {/* --- mid section --- */}
+        <div>
+            <p>COMPANY</p>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Privacy policy</li>
+            </ul> 
+        </div>
       </div>
     </div>
   )
