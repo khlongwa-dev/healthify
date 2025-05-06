1347c8074cecd35d95118f67146d5d28ba059968
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-06 15:14:22 +0200

    chore: added styling on the left section of the banner.

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -17,8 +17,8 @@ const Banner = () => {
       </div>
 
       {/*--- right side --- */}
-      <div>
-        <img src={assets.appointment_img} alt="" />
+      <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
+        <img className='w-full absolute bottom-0 right-0 max-w-md' src={assets.appointment_img} alt="" />
       </div>
     </div>
   )
