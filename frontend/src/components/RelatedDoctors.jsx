4bfdc88a2ec7adbd382c8eca77d5d63af7afd304
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-08 15:43:19 +0200

    fix: there was an incorrect variable name on related doctors component refering to related doctors.

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -19,7 +19,7 @@ const RelatedDoctors = ({speciality, doctorId}) => {
       <h1 className='text-3xl font-medium'>Related Doctors</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-        {relDocs.slice(0,5).map((item, index)=>(
+        {relatedDoctors.slice(0,5).map((item, index)=>(
             <div onClick={()=>{navigate(`/appointments/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} >
                 <img className='bg-[#fdf6ee]' src={item.image} alt="" />
                 <div className='p-4'>
