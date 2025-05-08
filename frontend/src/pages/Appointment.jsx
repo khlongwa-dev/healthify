fa81791605f194223aeba07d234beff72d8a11b5
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-08 14:48:19 +0200

    chore: set up all variables that will be required in the Appointments page.

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'next/navigation'
+import { AppContext } from '../context/AppContext'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
+  const {doctorId} = useParams()
+  const {doctors, currencySymbol} = useContext(AppContext)
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+
+  const [doctorInfo, setDoctorInfo] = useState(null)
+  const [doctorSlots, setDoctorSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
+
   return (
     <div>
       
