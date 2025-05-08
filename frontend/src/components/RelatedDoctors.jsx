85729ddc1600254acf9d7cb864ad0d2d9b14537a
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-08 14:25:51 +0200

    chore: apply use effect to filter the doctors with respect to speciality for related doctors component.

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
-const RelatedDoctors = () => {
+const RelatedDoctors = ({speciality, doctorId}) => {
+    const {doctors} = useContext(AppContext)
+    const [relatedDoctors, setRelatedDoctors] = useState([])
+    const navigate = useNavigate()
+
+    useNavigate(()=>{
+        if (doctors.length > 0 && speciality) {
+            const relatedDoctorsData = doctors.filter((doctor)=>doctor.speciality === doctor.speciality && doctor._id !== docId)
+            setRelatedDoctors(relatedDoctorsData)
+        }
+    }, [doctors, speciality, doctorId])
+    
   return (
     <div>
       
