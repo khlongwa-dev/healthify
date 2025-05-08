c291f63e4dfd00e4939455be7093f495975c462e
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-08 14:36:51 +0200

    chore: added styling on the related doctors component.

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -15,16 +15,16 @@ const RelatedDoctors = ({speciality, doctorId}) => {
     }, [doctors, speciality, doctorId])
 
   return (
-    <div>
-      <h1>Related Doctors</h1>
-      <p>Simply browse through our extensive list of trusted doctors.</p>
-      <div>
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+      <h1 className='text-3xl font-medium'>Related Doctors</h1>
+      <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
+      <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {relDocs.slice(0,5).map((item, index)=>(
-            <div onClick={()=>{navigate(`/appointments/${item._id}`); scrollTo(0,0)}} key={index} >
-                <img src={item.image} alt="" />
-                <div >
-                    <div>
-                        <p></p><p>Available</p>
+            <div onClick={()=>{navigate(`/appointments/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} >
+                <img className='bg-[#fdf6ee]' src={item.image} alt="" />
+                <div className='p-4'>
+                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
                     </div>
                     <p>{item.name}</p>
                     <p>{item.speciality}</p>
@@ -32,7 +32,7 @@ const RelatedDoctors = ({speciality, doctorId}) => {
             </div>
         ))}
       </div>
-      <button onClick={()=>{ navigate('/doctors'); scrollTo(0,0)}}>more</button>
+      <button onClick={()=>{ navigate('/doctors'); scrollTo(0,0)}} className='bg-[#fdf6ee] text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
