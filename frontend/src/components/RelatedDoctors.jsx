6d6862969610f22caf3da9365b3ffb392e06a05d
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-08 14:29:43 +0200

    feat: implemented the related doctors component.

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -13,10 +13,26 @@ const RelatedDoctors = ({speciality, doctorId}) => {
             setRelatedDoctors(relatedDoctorsData)
         }
     }, [doctors, speciality, doctorId])
-    
+
   return (
     <div>
-      
+      <h1>Related Doctors</h1>
+      <p>Simply browse through our extensive list of trusted doctors.</p>
+      <div>
+        {relDocs.slice(0,5).map((item, index)=>(
+            <div onClick={()=>{navigate(`/appointments/${item._id}`); scrollTo(0,0)}} key={index} >
+                <img src={item.image} alt="" />
+                <div >
+                    <div>
+                        <p></p><p>Available</p>
+                    </div>
+                    <p>{item.name}</p>
+                    <p>{item.speciality}</p>
+                </div>
+            </div>
+        ))}
+      </div>
+      <button onClick={()=>{ navigate('/doctors'); scrollTo(0,0)}}>more</button>
     </div>
   )
 }
