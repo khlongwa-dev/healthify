a15ba398c865c66ca4270e803c72da70b0a4d42e
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-08 15:15:55 +0200

    chore: added styling on the doctor information section of the appointment page.

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -79,28 +79,28 @@ const Appointment = () => {
   return doctorInfo && (
     <div>
       {/* ---- Doctor Details ----- */}
-      <div>
+      <div className='flex flex-col sm:flex-row gap-4'>
         <div>
-          <img src={docInfo.image} alt="" />
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
         </div>
         
         {/* ---- doc info, degree, exp etc. ---- */}
-        <div>
-          <p>
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
             {docInfo.name}
-            <img src={assets.verified_icon} alt="" />
+            <img className='w-5' src={assets.verified_icon} alt="" />
           </p>
-          <div>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
             <p>{docInfo.degree} - {docInfo.speciality}</p>
-            <button>{docInfo.experience}</button>
+            <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
           </div>
 
-          {/* ---- soc about ---- */}
+          {/* ---- doc about ---- */}
           <div>
-            <p>About <img src={assets.info_icon} alt="" /></p>
-            <p>{docInfo.about}</p>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
           </div>
-          <p>
+          <p className='text-gray-500 font-medium mt-4'>
             Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
           </p>
         </div>
