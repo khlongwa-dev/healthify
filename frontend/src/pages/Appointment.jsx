3e3e8d925e9450e9bab57b85e8c1dd9ecb097f51
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-08 14:53:10 +0200

    chore: added a function responsible for getting available slots on the appointments page.

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -4,8 +4,8 @@ import { AppContext } from '../context/AppContext'
 import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
-  const {doctorId} = useParams()
-  const {doctors, currencySymbol} = useContext(AppContext)
+  const { doctorId } = useParams()
+  const { doctors, currencySymbol } = useContext(AppContext)
   const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
   const [doctorInfo, setDoctorInfo] = useState(null)
@@ -18,9 +18,53 @@ const Appointment = () => {
     setDoctorInfo(doctorInfo)
   }
 
+  const getAvailableSlots = async () => {
+    setDoctorSlots([])
+
+    // getting correct date
+    let today = new Date()
+
+    for (let i = 0; i < 7; i++) {
+      // getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate() + i)
+
+      // setting end time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate() + i)
+      endTime.setHours(21, 0, 0, 0)
+
+      // setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+
+        // add slot to array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+
+        // increment current time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+
+      setDoctorSlots(prev => ([...prev, timeSlots]))
+    }
+  }
+
   return (
     <div>
-      
+
     </div>
   )
 }
