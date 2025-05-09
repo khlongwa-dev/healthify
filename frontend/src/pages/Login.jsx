4ed64561952f4170f5d1285521a17e3c41798c4e
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-09 18:28:37 +0200

    chore: setup variables (useState).

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [state, setState] = useState('Sign Up')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  
   return (
     <div>
       
