e6523a4d0b0e35f1c70d7e09cf78a348c2ae8d7e
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-09 18:34:15 +0200

    feat: implemented a login page.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,9 +11,37 @@ const Login = () => {
   }
   
   return (
-    <div>
-      
-    </div>
+    <form>
+      <div>
+        <p>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+        <p>Please {state === 'Sign Up' ? "sign up" : "login in"} to book appointment.</p>
+        
+        {
+          state === "Sign Up" &&
+          <div>
+            <p>Full Name</p>
+            <input type="text" onChange={(e)=>setName(e.target.name)} value={name}/>
+          </div>
+        }
+
+        <div>
+          <p>Email</p>
+          <input type="email" onChange={(e)=>setEmail(e.target.email)} value={email}/>
+        </div>
+
+        <div>
+          <p>Password</p>
+          <input type="password" onChange={(e)=>setPassword(e.target.password)} value={password}/>
+        </div>
+
+        <button>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        {
+          state === 'Sign Up'
+          ? <p>Already have an account? <span onClick={()=>setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p>
+          : <p>Create a new account? <span onClick={()=>setState('Sign Up')} className='text-primary underline cursor-pointer'>Click here</span></p>
+        }
+      </div>
+    </form>
   )
 }
 
