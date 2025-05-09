b7ad9ff83c3c76e5e801bfb26db28db697d1cd9a
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-09 17:59:55 +0200

    chore: added styling on contact information - user profile page.

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -26,13 +26,13 @@ const UserProfile = () => {
           ? <input className='bg-gray-50 text-3xl font-medium max-w-60 mt-4' type="text" value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} />
           : <p className='font-medium text-3xl text-neutral-800 mt-4'>{userData.name}</p>
       }
-      <hr/>
+      <hr className='bg-zinc-400 h-[1px] border-none' />
       <div>
-        <p>CONTACT INFORMATION</p>
-        <div>
-          <p>Email:</p>
-          <p>{userData.email}</p>
-          <p>Phone:</p>
+        <p className='text-neutral-500 underline mt-3'>CONTACT INFORMATION</p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
+          <p className='font-medium'>Email:</p>
+          <p className='text-blue-500'>{userData.email}</p>
+          <p className='font-medium'>Phone:</p>
 
           {
             isEdit
