70d4758dd6feb545bbf20b4c0b508016074a216e
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-09 18:25:45 +0200

    fix: fixed a styling issue on the background of doctor profile in user appointments page.

--- frontend/src/pages/UserAppointments.jsx
+++ frontend/src/pages/UserAppointments.jsx
@@ -11,7 +11,7 @@ const UserAppointments = () => {
         {doctors.slice(0,2).map((item, index)=>(
           <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
             <div>
-              <img className='w-32 bg-indigo-50' src={item.image} alt="" />
+              <img className='w-32 bg-[#fdf6ee]' src={item.image} alt="" />
             </div>
             <div className='flex-1 text-sm text-zinc-700'>
               <p className='text-neutral-800 font-semibold'>{item.name}</p>
