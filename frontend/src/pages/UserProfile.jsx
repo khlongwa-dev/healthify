5f48f3248cd88d566c93dbffbcf4eff6fc45082c
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-09 17:24:53 +0200

    chore: created a dummy data for a user.

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const UserProfile = () => {
+  const [userData, setUserData] = useState({
+    name: "Kusasalakhe Hlongwa",
+    image: assets.profile_pic,
+    email: 'ayandahlongwa21@gmail.com',
+    phone: '+27 63 384 8266',
+    address: {
+      line1: "31 Acutt Street",
+      line2: "Durban, South Africa"
+    },
+    gender: 'Male',
+    dob: '2001-04-03'
+  })
+
+  const [isEdit, setIsEdit] = useState(false)
   return (
     <div>
       
