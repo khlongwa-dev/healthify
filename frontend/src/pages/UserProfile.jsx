828cca989b596943e03a79912e6bde287e21e425
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-09 17:36:55 +0200

    feat(wip): making fields of a user profile editable.

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -26,6 +26,22 @@ const UserProfile = () => {
           ? <input type="text" value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} />
           : <p>{userData.name}</p>
       }
+      <hr/>
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+
+          {
+            isEdit
+              ? <input type="text" value={userData.phone} onChange={e => setUserData(prev => ({ ...prev, phone: e.target.value }))} />
+              : <p>{userData.phone}</p>
+          }
+          
+        </div>
+      </div>
       
     </div>
   )
