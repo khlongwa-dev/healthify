488c82b95911fafcba30d45034ac130c0c095853
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-09 18:19:28 +0200

    feat: added a mocked user appointments page.

--- frontend/src/pages/UserAppointments.jsx
+++ frontend/src/pages/UserAppointments.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const UserAppointments = () => {
+  const { doctors } = useContext(AppContext)
+
   return (
     <div>
-      
+      <p>My appointments</p>
+      <div>
+        {doctors.slice(0,2).map((item, index)=>(
+          <div key={index}>
+            <div>
+              <img src={item.image} alt="" />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p><span>Date & Time:</span> 25, July, 2024 | 08:30 PM</p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel appointment</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
