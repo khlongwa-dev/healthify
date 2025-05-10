26e380f00fcb7d161ba6e829a5e700134fed3e38
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-10 16:19:50 +0200

    feat: added a login page for admin.

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const Login = () => {
+
+    const [state, setState] = useState('Admin')
+
   return (
-    <div>
-      
-    </div>
+    <form>
+        <div>
+            <p><span> {state} </span> Login</p>
+            <div>
+                <p>Email</p>
+                <input type="email" required />
+            </div>
+            <div>
+                <p>Password</p>
+                <input type="password" required />
+            </div>
+            <button>Login</button>
+        </div>
+    </form>
   )
 }
 
