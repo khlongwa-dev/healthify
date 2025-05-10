7a14eceeee28ebe83f57855e5f2b6dc6f3dfb1be
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-10 16:01:52 +0200

    chore: added context provider on admin admin  context.

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react'
+
+export const AdminContext = createContext()
+
+const AdminContextProvider = (props) => {
+    const value = {
+        
+    }
+
+    return (
+        <AdminContext.Provider value= {value}>
+            {props.children}
+        </AdminContext.Provider>
+    )
+}
+
+export default AdminContextProvider
