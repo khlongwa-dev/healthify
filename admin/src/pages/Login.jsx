9213daac77b561fa0e389d17b7a807230497c855
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-10 16:27:42 +0200

    chore(wip): styling the admin login page.

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -6,9 +6,9 @@ const Login = () => {
     const [state, setState] = useState('Admin')
 
   return (
-    <form>
-        <div>
-            <p><span> {state} </span> Login</p>
+    <form className='min-h-[80vh] flex items-center'>
+        <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
+            <p className='text-2xl font-semibold m-auto'><span className='text-primary'> {state} </span> Login</p>
             <div>
                 <p>Email</p>
                 <input type="email" required />
