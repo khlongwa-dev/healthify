8f726c6400d1cab7f4edb3231cf7cb0e7e38b00e
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 17:24:53 +0200

    feat(wip): added styling for the navbar logo and role.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
 
     const {aToken} = useContex(AdminContext)
   return (
-    <div>
-      <div>
-        <img src={assets.admin_logo} alt="" />
-        <p>{ aToken ? 'Admin' : 'Doctor'}</p>
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+      <div className='flex items-center gap-2 text-xs'>
+        <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+        <p className='border px-2.5 py-0.5 rounded-full border-gray-600'>{ aToken ? 'Admin' : 'Doctor'}</p>
       </div>
       <button>Logout</button>
     </div>
