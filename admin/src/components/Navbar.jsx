d767cbd472b3ce2a95f86793e500e40859b9f388
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 17:10:20 +0200

    feat(wip): developing a navbar for the admin.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
 
 const Navbar = () => {
+
+    const {aToken} = useContex(AdminContext)
   return (
     <div>
-      
+      <div>
+        <img src={assets.admin_logo} alt="" />
+        <p>{ aToken ? 'Admin' : 'Doctor'}</p>
+      </div>
+      <button>Logout</button>
     </div>
   )
 }
