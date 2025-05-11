b8a177bcbb532773e68b9e8cfda27823c5b930ad
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 20:43:53 +0200

    chore(wip): styling the add doctor page.

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -7,32 +7,32 @@ const AddDoctor = () => {
       <p className='mb-3 text-lg font-medium'>Add a doctor</p>
 
       <div className='bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll'>
-        <div>
+        <div className='flex items-center gap-4 mb-8 text-gray-500'>
           <label htmlFor="doc-img">
-            <img src={assets.upload_area} alt="" />
+            <img className='w-16 bg-gray-100 rounded-full cursor-pointer' src={assets.upload_area} alt="" />
           </label>
           <input type="file" id="doc-img" hidden/>
           <p>Upload doctor <br /> picture</p>
         </div>
 
-        <div>
-          <div>
-            <div>
+        <div className='flex flex-col lg:flex-row items-start gap-10 text-gray-600'>
+          <div className='w-full lg:flex-1 flex flex-col  gap-4'>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Doctor name</p>
               <input type="text" placeholder='Name' required />
             </div>
 
-            <div>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Doctor Email</p>
               <input type="email" placeholder='Email' required />
             </div>
 
-            <div>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Doctor Password</p>
               <input type="password" placeholder='Password' required />
             </div>
 
-            <div>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Experience</p>
               <select name="" id="">
                 <option value="1 Year">1 Year</option>
@@ -48,14 +48,14 @@ const AddDoctor = () => {
               </select>
             </div>
 
-            <div>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Fees</p>
               <input type="number" placeholder='Fees' required />
             </div>
           </div>
           
-          <div>
-            <div>
+          <div className='w-full lg:flex-1 flex flex-col gap-4'>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Speciality</p>
               <select name="" id="">
                 <option value="General Physician">General Physician</option>
@@ -67,12 +67,12 @@ const AddDoctor = () => {
               </select>
             </div>
 
-            <div>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Education</p>
               <input type="text" placeholder='Education' required />
             </div>
 
-            <div>
+            <div className='flex-1 flex flex-col gap-1'>
               <p>Address</p>
               <input type="text" placeholder='line 1' required/>
               <input type="text" placeholder='line 2' required/>
