8f81523bd7b60e382ba54603636a7fdc6bcd1e32
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 19:48:17 +0200

    feat(wip): developing add a doctor page.

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
   return (
-    <div>
-      
-    </div>
+    <form>
+      <p>Add a doctor</p>
+      <div>
+        <div>
+          <label htmlFor="doc-img">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="doc-img" hidden/>
+          <p>Upload doctor <br /> picture</p>
+        </div>
+      </div>
+    </form>
   )
 }
 
