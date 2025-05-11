4744d58c75664b7600c26f8adb288d84b3231d19
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 19:33:06 +0200

    feat(wip): styling the side bar.

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,10 +7,10 @@ const Sidebar = () => {
     const {aToken} = useContext(AdminContext)
 
   return (
-    <div>
+    <div className='min-h-screen bg-white border-r'>
       {
-        aToken && <ul>
-            <NavLink to={'/admin-dashboard'}>
+        aToken && <ul className='text-[#515151] mt-5'>
+            <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary': ''}`} to={'/admin-dashboard'}>
                 <img src={assets.home_icon} alt="" />
                 <p>Dashboard</p>
             </NavLink>
