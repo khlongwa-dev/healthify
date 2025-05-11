149933ad05c85a6c23b187ff6a5baa94cd7de268
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 18:10:26 +0200

    chore: added routes for admin panel.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,11 @@ import 'react-toastify/dist/ReactToastify.css'
 import { AdminContext } from './context/AdminContext'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Admin/Dashboard'
+import AddDoctor from './pages/Admin/AddDoctor'
+import AllAppointments from './pages/Admin/AllAppointments'
+import DoctorsList from './pages/Admin/DoctorsList'
 
 const App = () => {
   const {aToken} = useContext(AdminContext)
@@ -16,6 +20,13 @@ const App = () => {
       <Navbar />
       <div className='flex items-start'>
         <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='all-appointments' element={<AllAppointments/>}/>
+          <Route path='doctors-list' element={<DoctorsList/>}/>
+        </Routes>
       </div>
     </div>
   ):(
