f296f92ffd844771ea47860d1514f5cab772a2a4
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 17:43:04 +0200

    chore: add a logouut function to a logout button on a navbar admin panel.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,14 +4,20 @@ import { AdminContext } from '../context/AdminContext'
 
 const Navbar = () => {
 
-    const {aToken} = useContext(AdminContext)
+    const {aToken, setAToken} = useContext(AdminContext)
+
+    const logout = () => {
+        aToken && setAToken('')
+        aToken && localStorage.removeItem('aToken')
+    }
+
   return (
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
       <div className='flex items-center gap-2 text-xs'>
         <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
         <p className='border px-2.5 py-0.5 rounded-full border-gray-600'>{ aToken ? 'Admin' : 'Doctor'}</p>
       </div>
-      <button className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+      <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
     </div>
   )
 }
