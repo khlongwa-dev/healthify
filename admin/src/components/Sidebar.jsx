b3898da414da6efb6af1df22e8320920773e2b5a
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-11 19:22:59 +0200

    chore: added 'to' property on  navigation links on an admin panel sidebar.

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -10,22 +10,22 @@ const Sidebar = () => {
     <div>
       {
         aToken && <ul>
-            <NavLink>
+            <NavLink to={'/admin-dashboard'}>
                 <img src={assets.home_icon} alt="" />
                 <p>Dashboard</p>
             </NavLink>
 
-            <NavLink>
+            <NavLink to={'/all-appointments'}>
                 <img src={assets.appointment_icon} alt="" />
                 <p>Appointments</p>
             </NavLink>
 
-            <NavLink>
+            <NavLink to={'/add-doctor'}>
                 <img src={assets.add_icon} alt="" />
                 <p>Add Doctor</p>
             </NavLink>
 
-            <NavLink>
+            <NavLink to={'/doctors-list'}>
                 <img src={assets.people_icon} alt="" />
                 <p>Doctors List</p>
             </NavLink>
