3706b9ba29a16eed0f1022a43fd46215068264c9
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-13 00:16:53 +0200

    chore: called an api on app context to get user data.

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,7 +9,8 @@ const AppContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [doctors, setDoctors] = useState([])
     
-    const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'):false)
+    const [userData, setUserData] = useState(false)
 
 
     const getDoctorsData = async () => {
@@ -27,16 +28,41 @@ const AppContextProvider = (props) => {
         }
     }
 
+    const loadUserProfileData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + 'api/user/get-profile', {headers:{token}})
+
+            if (data.success) {
+                setUserData(data.user)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         doctors,
         currencySymbol,
         token, setToken,
-        backendUrl
+        backendUrl,
+        userData, setUserData,
+        loadUserProfileData
     }
 
     useEffect(()=>{
         getDoctorsData()
     },[])
+
+    useEffect(()=>{
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+    },[token])
 
     return (
         <AppContext.Provider value={value}>
