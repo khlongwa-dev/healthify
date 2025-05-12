e8103634fb3435fce8dd84c2d969ba11b7e29abf
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-12 13:42:41 +0200

    feat: implementing a doctor endpoint for updating availability.

---
+++ backend/Controllers/DoctorController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+using backend.Data;
+using backend.DTOs;
+using backend.Services;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DoctorController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly JwtService _jwt;
+
+    public DoctorController(AppDbContext context, JwtService jwt)
+    {
+        _context = context;
+        _jwt = jwt;
+    }
+
+    [HttpPost("change-availability")]
+    public async Task<IActionResult> ChangeAvailability([FromForm] DoctorDto dto)
+    {
+        
+    }
+}
