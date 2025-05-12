21d971d4dcba2991ce0a9eb4dc77cdb8cd1e14ec
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-12 17:38:26 +0200

    chore: added an api call on the frontend app context to get doctors data.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,13 @@ import About from './pages/About'
 import Contacts from './pages/Contacts'
 import Appointment from './pages/Appointment'
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer/>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
