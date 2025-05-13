65eca6c17fa0ecc4c1ceda65f41da8556bce1144
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-13 13:01:20 +0200

    chore: added styling on the image edit section of user proile.

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -17,9 +17,9 @@ const UserProfile = () => {
       {
         isEdit
         ? <label htmlFor='image'>
-            <div>
-              <img src={image ? URL.createObjectURL(image):userData.imageUrl} alt="" />
-              <img src={image ? '': assets.upload_icon} alt="" />
+            <div className='inline-block relative cursor-pointer'>
+              <img className='w-36 rounded opacity-75' src={image ? URL.createObjectURL(image):userData.imageUrl} alt="" />
+              <img className='w-10 absolute bottom-12 right-12' src={image ? '': assets.upload_icon} alt="" />
             </div>
             <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden/>
         </label>
