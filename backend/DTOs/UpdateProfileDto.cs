37c4c69f334c5632383d400849aa6c3190876988
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-13 13:28:20 +0200

    chore: added a profile update dto.

---
+++ backend/DTOs/UpdateProfileDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs
+{
+    public class UpdateUserProfileDto
+    {
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Phone { get; set; }
+        public required string AddressLine1 { get; set; }
+        public required string AddressLine2 { get; set; }
+        public required string Gender { get; set; }
+
+        [Required]
+        public DateOnly DoB { get; set; }
+
+        // Optional image (form field name must match)
+        public IFormFile? ImageUrl { get; set; }
+    }
+}
