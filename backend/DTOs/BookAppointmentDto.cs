42dcc4465990a3a9c703e705d59930e9d7aa8456
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-13 21:33:03 +0200

    fix: fixed a bad request error by updating the datatype of docId in booking dto.

--- backend/DTOs/BookAppointmentDto.cs
+++ backend/DTOs/BookAppointmentDto.cs
@@ -4,7 +4,7 @@ namespace backend.DTOs
 {
     public class BookingAppointmentDto
     {
-        public int DoctorId { get; set; }
+        public required string DoctorId { get; set; }
         public required string SlotDate { get; set; }
         public required string SlotTime { get; set; }
     
