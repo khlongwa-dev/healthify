aa90aeaee9cea673941204ff1114b1bfdf1fcf4f
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-13 12:39:53 +0200

    fix: fixed the incorrect hover syntax in user profile buttons.

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -68,8 +68,8 @@ const UserProfile = () => {
       <div className='mt-10'>
           {
             isEdit
-            ? <button className='border border-primary px-8 py-2 rounded-full hover: bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(false)} >Save Information</button>
-            : <button className='border border-primary px-8 py-2 rounded-full hover: bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(true)} >Edit</button>
+            ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(false)} >Save Information</button>
+            : <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(true)} >Edit</button>
           }
       </div>
     </div>
