12e3b952cad22a5fdeeaf1fe4c032888361a39ad
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-14 23:35:27 +0200

    chore: implemented a logic for formatting data on admin dashboard.

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from 'react'
 import { AdminContext } from '../../context/AdminContext'
 import { assets } from '../../assets/assets'
+import { AppContext } from '../../context/AppContext'
 
 const Dashboard = () => {
 
   const {aToken, getDashData, cancelAppointment, dashData} = useContext(AdminContext)
+  const {slotDateFormat} = useContext(AppContext)
 
   useEffect(()=>{
     if (aToken) {
@@ -54,7 +56,7 @@ const Dashboard = () => {
                 <img className='rounded-full w-10' src={item.doctor.imageUrl} alt="" />
                 <div className='flex-1 text-sm'>
                   <p className='text-gray-800 font-medium'>{item.doctor.name}</p>
-                  <p className='text-gray-600'>{item.slotDate}</p>
+                  <p className='text-gray-600'>{slotDateFormat(item.slotDate)}</p>
                 </div>
                 {
                   item.cancelled
