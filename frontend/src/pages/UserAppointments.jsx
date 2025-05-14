cd83b4147bc55efe902d040c71d1425580809068
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-14 13:58:45 +0200

    chore: implemented a logic for hiding a cancel button on an appointment already cancelled.

--- frontend/src/pages/UserAppointments.jsx
+++ frontend/src/pages/UserAppointments.jsx
@@ -69,7 +69,7 @@ const UserAppointments = () => {
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
               <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
-              <button onClick={()=>cancelAppointment(item.id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>
+              {!item.cancelled && <button onClick={()=>cancelAppointment(item.id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
             </div>
           </div>
         ))}
