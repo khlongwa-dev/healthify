253f33ec4da3ee117eedd836f38170c39a4415ff
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-14 21:59:41 +0200

    chore: implemented api call for getting admin dashboard data.

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,6 +8,9 @@ const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+
+    const [dashData, setDashData] = useState(false)
+
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async () => {
@@ -73,6 +76,18 @@ const AdminContextProvider = (props) => {
         } catch (error) {
             console.log(error)
             toast.error(error.message)
+        }
+    }
+
+    const getDashData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + 'api/admin/dashboard', {headers:{aToken}})
+
+            if (data.success) {
+                setDashData(data.dashdata)
+            }
+        } catch (error) {
+            
         }
     }
 
