1d852d9eba841bc7df1f0aefe0f2beefd48106d4
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-14 16:09:31 +0200

    chore: added html structure for all apointments admin page.

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
+import { useEffect } from 'react'
 
 const AllAppointments = () => {
+  const {aToken, appoinments, getAllAppointments} = useContext(AdminContext)
+
+  useEffect(()=>{
+    if (aToken) {
+      getAllAppointments()
+    }
+  }, [aToken])
+
   return (
     <div>
-      
+      <p>All appoinments</p>
+      <div>
+        <div>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Actions</p>
+        </div>
+      </div>
     </div>
   )
 }
