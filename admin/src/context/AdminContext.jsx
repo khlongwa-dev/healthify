90e3ac14adc15b2380b6df85676ed4f5cd7fc738
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-14 18:29:14 +0200

    chore: implemented api call for admin to cancel appointments.

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -59,13 +59,30 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const cancelAppointment = async (appointmentId) => {
+        try {
+            const {data} = await axios.post(backendUrl, 'api/admin/cancel-appointment', {appointmentId}, {header:{aToken}})
+
+            if(data.success) {
+                toast.success(data.message)
+                getAllAppointments()
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken, setAToken,
         backendUrl, doctors,
         getAllDoctors, changeAvailability,
         setAppointments,
         appointments,
-        getAllAppointments
+        getAllAppointments,
+        cancelAppointment
     }
 
     return (
