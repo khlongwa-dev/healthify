b4a0fc496c6b008d806700096fc9d1347d6e584d
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-15 00:46:19 +0200

    chore: added doctor routes.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,6 +11,9 @@ import AddDoctor from './pages/Admin/AddDoctor'
 import AllAppointments from './pages/Admin/AllAppointments'
 import DoctorsList from './pages/Admin/DoctorsList'
 import { DoctorContext } from './context/DoctorContext'
+import DoctorDashboard from './pages/Doctor/DoctorDashboard'
+import DoctorAppointments from './pages/Doctor/DoctorAppointments'
+import DoctorProfile from './pages/Doctor/DoctorProfile'
 
 const App = () => {
   const {aToken} = useContext(AdminContext)
@@ -23,12 +26,18 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar/>
         <Routes>
+          {/* Admin Routes */}
           {/* <Route path='/' element={<></>}/> */}
           {/* <Route path='/admin-dashboard' element={<Dashboard/>}/> */}
           <Route path='/' element={<Dashboard/>}/>
           <Route path='/add-doctor' element={<AddDoctor/>}/>
           <Route path='all-appointments' element={<AllAppointments/>}/>
           <Route path='doctors-list' element={<DoctorsList/>}/>
+
+          {/* Doctor Routes */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard/>} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments/>} />
+          <Route path='/doctor-profile' element={<DoctorProfile/>} />
         </Routes>
       </div>
     </div>
