053bf6fa7727d163e2d9b92cc22449326023c980
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-14 17:19:28 +0200

    chore(wip): styling the all appointments admin page.

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -26,8 +26,11 @@ const AllAppointments = () => {
           <p>Actions</p>
         </div>
         {appointments.map((item, index)=>(
-            <div key={index}>
-              <p>{index+1}</p>
+            <div className='flex flex-wrap justify-between max-sm:gap-2 sm:grid sm:grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
+              <p className='max-sm:hidden'>{index+1}</p>
+              <div>
+                <img src={item.user.imageUrl} alt="" /> <p>{item.user.name}</p>
+              </div>
             </div>
           ))}
       </div>
