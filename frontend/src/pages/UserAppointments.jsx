7bd90d2b443be3d94a7738de34e524f0421b9d8a
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-14 14:04:54 +0200

    feat: added a cancelled appointment button.

--- frontend/src/pages/UserAppointments.jsx
+++ frontend/src/pages/UserAppointments.jsx
@@ -70,6 +70,7 @@ const UserAppointments = () => {
             <div className='flex flex-col gap-2 justify-end'>
               {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
               {!item.cancelled && <button onClick={()=>cancelAppointment(item.id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
+              {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
             </div>
           </div>
         ))}
