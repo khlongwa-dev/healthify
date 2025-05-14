a675d51f314bf4f425e1e2fccec9d6aa65ee4857
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-15 01:33:01 +0200

    chore: implemented api call for geting doctor appointsment in doctor admin.

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,14 +1,32 @@
 import React, { createContext, useState } from 'react'
+import { toast } from 'react-toastify'
 
 export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props) => {
     const [dToken, setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
+    const [appointments, setAppointments] = useState([])
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const getDoctorAppointments = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + 'api/doctor/appointments', {headers:{dToken}})
+            if (data.success) {
+                setAppointments(data.appointments)
+                console.log(data.appointments)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
     
     const value = {
         dToken, setDToken,
-        backendUrl
+        backendUrl, appointments,
+        setAppointments, getDoctorAppointments
     }
 
     return (
--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useContext } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
+import { useEffect } from 'react'
 
 const DoctorAppointments = () => {
+
+  const { dToken, appointments, getDoctorAppointments } = useContext(DoctorContext)
+
+  useEffect (()=>{
+    if(dToken) {
+      getDoctorAppointments()
+    }
+  }, [dToken])
+  
   return (
     <div>
       
