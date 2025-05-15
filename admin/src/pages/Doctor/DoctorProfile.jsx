18a24c67db29ab02d7894f82edc9a24fb8fb3156
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-15 16:51:26 +0200

    chore: implemented api call for updating doctor profile.

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -5,6 +5,35 @@ import { DoctorContext } from '../../context/DoctorContext'
 
 const DoctorProfile = () => {
   const {dToken, loadDoctorProfileData} = useContext(DoctorContext)
+  const [isEdit, setIsEdit] = useState(false)
+  const [image, setImage] = useState(false)
+
+  const updateDoctorProfileData = async () => {
+    try {
+      const formData = new FormData()
+      formData.append('Phone', userData.phone)
+      formData.append('Name', userData.name)
+      formData.append('AddressLine1', userData.addressLine1)
+      formData.append('AddressLine2', userData.addressLine2)
+      formData.append('Gender', userData.gender)
+      formData.append('DoB', userData.doB)
+      
+      image && formData.append('ImageUrl', image)
+      const {data} = await axios.post(backendUrl + 'api/doctor/update-profile', formData, {headers:{token}})
+
+      if (data.success) {
+        toast.success(data.message)
+        await loadDoctorProfileData()
+        setIsEdit(false)
+        setImage(false)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
 
   useEffect(()=>{
     if(dToken) {
