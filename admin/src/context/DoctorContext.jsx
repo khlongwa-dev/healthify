7dbccec554576f1281ddb9420111d970556182fd
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-15 15:03:52 +0200

    added logic for hiding cancel and complete buttons when appointment is complete or cancelled.

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -29,7 +29,7 @@ const DoctorContextProvider = (props) => {
     const completeAppointment = async (appointmentId) => {
         try {
             
-            const {data} = await axios.get(backendUrl + 'api/doctor/complete-appointment', {appointmentId}, {headers:{dToken}})
+            const {data} = await axios.post(backendUrl + 'api/doctor/complete-appointment', {appointmentId}, {headers:{dToken}})
             if (data.success) {
                 toast.success(data.message)
                 getDoctorAppointments()
@@ -45,7 +45,7 @@ const DoctorContextProvider = (props) => {
     const cancelAppointment = async (appointmentId) => {
         try {
             
-            const {data} = await axios.get(backendUrl + 'api/doctor/cancel-appointment', {appointmentId}, {headers:{dToken}})
+            const {data} = await axios.post(backendUrl + 'api/doctor/cancel-appointment', {appointmentId}, {headers:{dToken}})
             if (data.success) {
                 toast.success(data.message)
                 getDoctorAppointments()
--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -10,12 +10,12 @@ const DoctorAppointments = () => {
   const { dToken, appointments, getDoctorAppointments, completeAppointment, cancelAppointment } = useContext(DoctorContext)
   const { calculateAge, slotDateFormat, currency } = useContext(AppContext)
 
-  useEffect (()=>{
-    if(dToken) {
+  useEffect(() => {
+    if (dToken) {
       getDoctorAppointments()
     }
   }, [dToken])
-  
+
   return (
     <div className='w-full max-w-6xl m-5'>
       <p className='mb-3 text-lg font-medium'>All Appointments</p>
@@ -30,24 +30,32 @@ const DoctorAppointments = () => {
           <p>Actions</p>
         </div>
         {
-          appointments.map((item, index)=>(
+          appointments.map((item, index) => (
             <div className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
-              <p className='max-sm:hidden'>{index+1}</p>
+              <p className='max-sm:hidden'>{index + 1}</p>
               <div className='flex items-center gap-2'>
                 <img className='w-8 rounded-full' src={item.user.imageUrl} alt="" /> <p>{item.user.name}</p>
               </div>
               <div>
-                <p  className='text-sm inline border border-primary px-2 rounded-full'>
+                <p className='text-sm inline border border-primary px-2 rounded-full'>
                   {item.paid ? 'Online' : 'CASH'}
                 </p>
               </div>
               <p className='max-sm:hidden'>{calculateAge(item.user.doB)}</p>
               <p>{slotDateFormat(item.slotDate)}, {item.slotTime}</p>
               <p>{currency}{item.doctorFee}</p>
-              <div className='flex'>
-                <img onClick={()=>cancelAppointment(item.id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
-                <img onClick={()=>completeAppointment(item.id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
-              </div>
+              {
+                item.cancelled
+                  ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
+                  : item.isCompleted
+                    ? <p className='text-green-500 text-xs font-medium'>Completed</p>
+                    :
+                    <div className='flex'>
+                      <img onClick={() => cancelAppointment(item.id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                      <img onClick={() => completeAppointment(item.id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
+                    </div>
+              }
+
             </div>
           ))
         }
