19743cde1b2d1402de760c1e25461fadf6171c58
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-15 12:34:31 +0200

    chore(wip): styling the all appointments page in doctor dashboard.

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -38,17 +38,17 @@ const Sidebar = () => {
         dToken && <ul className='text-[#515151] mt-5'>
             <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#fdf6ee] border-r-4 border-primary': ''}`} to={'/doctor-dashboard'}>
                 <img src={assets.home_icon} alt="" />
-                <p>Dashboard</p>
+                <p className='hidden md:block'>Dashboard</p>
             </NavLink>
 
             <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#fdf6ee] border-r-4 border-primary': ''}`} to={'/doctor-appointments'}>
                 <img src={assets.appointment_icon} alt="" />
-                <p>Appointments</p>
+                <p className='hidden md:block'>Appointments</p>
             </NavLink>
 
             <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#fdf6ee] border-r-4 border-primary': ''}`} to={'/doctor-profile'}>
                 <img src={assets.people_icon} alt="" />
-                <p>Profile</p>
+                <p className='hidden md:block'>Profile</p>
             </NavLink>
         </ul>
       }
