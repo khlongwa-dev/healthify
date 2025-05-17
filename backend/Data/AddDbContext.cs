7493664c3471f3ca119d41378a82359ed1e99988
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-17 22:31:44 +0200

    refactor: added UserAppointments table on app database context.

--- backend/Data/AddDbContext.cs
+++ backend/Data/AddDbContext.cs
@@ -11,6 +11,7 @@ namespace backend.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<UserAppointment> UserAppointments { get; set; }
         public DbSet<BookedSlot> BookedSlots { get; set; } 
     }
 }
