ace823614d2d4d367617c63c646335fdcb228053
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-17 23:57:47 +0200

    refactor: renamed DoctorDto to CreateDoctorDto.

--- backend/DTOs/CreateDoctorDto.cs
+++ backend/DTOs/CreateDoctorDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace backend.DTOs
 {
-    public class DoctorDto
+    public class CreateDoctorDto
     {
         public int Id { get; set; }
         public required string Name { get; set; }
