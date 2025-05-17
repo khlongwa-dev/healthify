cece3d716a10767bed35ceba82445ef3ae4808bb
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-17 22:55:03 +0200

    refactor: changed DoctorId in BookAppointmentDto from datatype string to datatype int.

--- backend/DTOs/BookAppointmentDto.cs
+++ backend/DTOs/BookAppointmentDto.cs
@@ -4,7 +4,7 @@ namespace backend.DTOs
 {
     public class BookingAppointmentDto
     {
-        public required string DoctorId { get; set; }
+        public int DoctorId { get; set; }
         public required string SlotDate { get; set; }
         public required string SlotTime { get; set; }
     
