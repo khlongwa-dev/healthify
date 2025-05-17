1ebcdead9207bb2632a402dcfbd8fedd4632264b
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-17 22:22:22 +0200

    refactor: implemented GetUserByIdAsync for UserService.

--- backend/Services/Implementations/UserServices.cs
+++ backend/Services/Implementations/UserServices.cs
@@ -38,5 +38,11 @@ namespace backend.Services.Implementations
         }
 
 
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        
     }
 }
