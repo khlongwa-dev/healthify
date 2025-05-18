8f6102a14a07a1b7e6711be4ddef719495dbdf79
Kusasalakhe Hlongwa <ayandahlongwa21@gmail.com>
2025-05-18 14:53:16 +0200

    refactor: refactored admin application Doctor page DoctorDashboard to use a refactored doctor context.

--- admin/src/pages/Doctor/DoctorDashboard.jsx
+++ admin/src/pages/Doctor/DoctorDashboard.jsx
@@ -5,23 +5,25 @@ import { AppContext } from '../../context/AppContext'
 
 const DoctorDashboard = () => {
 
-  const { dToken, dashData, setDashData, getDashboardData, completeAppontment, cancelAppointment } = useContext(DoctorContext)
-  const { currency, slotDateFormat } = useContext(AppContext)
+  const { doctorToken, dashboardStats, setDashboardStats, fetchDashboardStats, completeAppointment, cancelAppointment } = useContext(DoctorContext)
+  const { currencySymbol, formatSlotDate } = useContext(AppContext)
 
-  useEffect(() => {
-    if (dToken) {
-      getDashboardData()
-    }
-  }, [dToken])
+  useEffect(() => 
+    {
+      if (doctorToken)
+      {
+        fetchDashboardStats()
+      }
+    }, [doctorToken])
 
-  return dashData && (
+  return dashboardStats && (
     <div className='m-5'>
       <div className='flex flex-wrap gap-3'>
 
         <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
           <img className='w-14' src={assets.earning_icon} alt="" />
           <div>
-            <p className='text-x1 font-semibold text-gray-600'>{currency} {dashData.earnings}</p>
+            <p className='text-x1 font-semibold text-gray-600'>{currencySymbol} {dashboardStats.earnings}</p>
             <p className='text-gray-400'>Earnings</p>
           </div>
         </div>
@@ -29,7 +31,7 @@ const DoctorDashboard = () => {
         <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
           <img className='w-14' src={assets.appointments_icon} alt="" />
           <div>
-            <p className='text-x1 font-semibold text-gray-600'>{dashData.patients}</p>
+            <p className='text-x1 font-semibold text-gray-600'>{dashboardStats.appointmentCount}</p>
             <p className='text-gray-400'>Appointments</p>
           </div>
         </div>
@@ -37,7 +39,7 @@ const DoctorDashboard = () => {
         <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
           <img className='w-14' src={assets.patients_icon} alt="" />
           <div>
-            <p className='text-x1 font-semibold text-gray-600'>{dashData.userCount}</p>
+            <p className='text-x1 font-semibold text-gray-600'>{dashboardStats.patientCount}</p>
             <p className='text-gray-400'>Patients</p>
           </div>
         </div>
@@ -51,12 +53,12 @@ const DoctorDashboard = () => {
 
         <div className='pt-4 border border-t-0'>
           {
-            dashData.latestAppointments.map((item, index) => (
+            dashboardStats.latestAppointments.map((item, index) => (
               <div className='flex items-center px-6 py-3 hover:bg-gray-100' key={index}>
                 <img className='rounded-full w-10' src={item.user.imageUrl} alt="" />
                 <div className='flex-1 text-sm'>
                   <p className='text-gray-800 font-medium'>{item.user.name}</p>
-                  <p className='text-gray-600'>{slotDateFormat(item.slotDate)}</p>
+                  <p className='text-gray-600'>{formatSlotDate(item.slotDate)}</p>
                 </div>
                 {
                   item.cancelled
